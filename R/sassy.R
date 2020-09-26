7dccb343641f8bdf503832e4a3ee3bf67a8b23e5
David Bosak <dbosak01@gmail.com>
2020-09-25 21:16:42 -0400

    Initial commit.

---
+++ R/sassy.R
@@ -0,0 +1,28 @@
+#' @title A Collection of Packages Inspired by SAS®
+#'
+#' @description The \strong{sassy} package is a meta-package that
+#' installs a set of packages
+#' inspired by SAS® software.  These packages include the ability to create
+#' a traceable log, create data libraries and format catalogs, and includes
+#' reporting capabilities similar to those found in SAS®.  This package was
+#' written independently, and the authors have no association with or
+#' endorsement of SAS Institute.
+#'
+#' @section Packages Included:
+#' The packages included in the \strong{sassy} meta-package are
+#' as follows:
+#' \itemize{
+#'   \item {\code{\link[logr]{logr}}:}{ Write a traceable log.}
+#'   \item {\code{\link[fmtr]{fmtr}}:}{ Create a format catalog
+#' and apply formats to a data frame or vector.}
+#   \item {\code{\link[libr]{libr}}:}{ Define a libname and generate
+#   data dictionaries.}
+#   \item {\code{\link[rptr]{rptr}}:}{ Write reports.}
+#   \item {\code{\link[sasr]{sasr}}:}{ Access the SAS® system from R.}
+#' }
+#'
+#' @docType package
+#' @name sassy
+#' @import logr
+#' @import fmtr
+NULL
---
+++ README.md
@@ -0,0 +1,16 @@
+# sassy <img src="./man/images/sassy.jpg" align="right" height="138" />
+
+<!-- badges: start -->
+
+[![sassy version](https://www.r-pkg.org/badges/version/sassy)](https://cran.r-project.org/package=sassy)
+[![sassy lifecycle](https://img.shields.io/badge/lifecycle-maturing-blue.svg)](https://cran.r-project.org/package=sassy)
+[![sassy downloads](https://cranlogs.r-pkg.org/badges/grand-total/sassy)](https://cran.r-project.org/package=sassy)
+[![Travis build status](https://travis-ci.com/dbosak01/sassy.svg?branch=master)](https://travis-ci.com/dbosak01/sassy)
+
+<!-- badges: end -->
+
+  The **sassy** package is a meta-package that aims to bring several SAS
+concepts to R.  These concepts include data sassyaries, format catalogs,
+data dictionaries,
+a traceable log, and a reporting package inspired by PROC REPORT and ODS.
+
